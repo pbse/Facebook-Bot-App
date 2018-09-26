5b3f3d0206122d0a9a8d1d71194235a022438bd9
Prashant <pbansal@liveperson.com>
2018-09-25 23:12:35 -0400

    Updates

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,18 +1,10 @@
-"use strict";
-
-//import request from "request";
 import express from "express";
 const router = express.Router()
 import logger from "../util/logger";
 
-router.get('/webhook/', (req: express.Request, res: express.Response) => {
-    if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
-        logger.info({"module": "Home Controller", "message": "Webhook Verification Successful"});
-        return res.send(req.query['hub.challenge'])
-    }
-    logger.info({"module": "Home Controller", "message": "Webhook Verification Failed"});
-    return res.send('wrong token')
-})
-
+router.get('/', (req: express.Request, res: express.Response) => {
+    logger.info({"module": "Home Controller", "message": "Some One Called Me", "details" : req});
+    res.send('Thanks for Checking Us Out');
+});
 
 export default router;
