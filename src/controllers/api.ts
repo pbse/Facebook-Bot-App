05bbdc21f508911b060e54de1cda0708f0a3d747
Prashant <pbansal@liveperson.com>
2018-10-01 00:39:52 -0400

    Update Comments

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,6 +1,4 @@
 "use strict";
-
-//import request from "request";
 import express from "express";
 const router = express.Router()
 import logger from "../util/logger";
@@ -17,17 +15,34 @@ const bot = new BootBot({
     appSecret: process.env.APP_SECRET
 });
 
-// define the home page route
+/**
+ * Define Home page route
+ * Not using this here though
+ */
 router.get('/', function (_req, res) {
     res.send('Thanks for Checking Us Out');
 });
 
+/**
+ * This will get triggered as and when any one
+ * opens the chat window for the first time
+ */
 bot.setGreetingText("Hello, Welcome to XYZ Page. Lets start by knowing your favorite genre.");
 
+/**
+ * Log the message we receive.
+ * All the message with type `message`
+ * will appear in this function
+ */
 bot.on('message', (payload : Payload, _chat : Chat) => {
     logger.info({"module": "Api Controller", "message": payload.message.text, "details": payload});
 });
 
+/**
+ * Log the Attachment message we receive.
+ * All the message with type `Attachment message`
+ * will appear in this function
+ */
 bot.on('attachment', (payload : Payload, chat : Chat) => {
     logger.info({"module": "Api Controller", "message": "Recieved Attachment", "details": payload});
     chat.say('We do not support this message type');
